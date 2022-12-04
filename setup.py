41d194567322770b6d764765448ed86d7167103c
Hamed-kalak <119797874+Hamed-kalak@users.noreply.github.com>
2022-12-04 21:28:25 +0100

    the setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutlis.core import setup
+from setuptools import find_packages
+
+setup(
+    name='snowflake',
+    version='0.1',
+    author='HAMED',
+    author_email='hamed.mirbagher@fau.de',
+    packages=find_packages(),
+    install_requires=['numpy','turtles'],
+    )
